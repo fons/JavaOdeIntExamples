394b07f10c5a0b86518493588325adbe194456ac
fons haffmans <fons.haffmans@gmail.com>
2016-09-28 18:50:25 -0400

    executable jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        //LogisticGrowthExample.run();
+        LogisticGrowthExample.run();
 
         //lorenz model
-        //LorentzModel.run();
+        LorentzModel.run();
 
         // rigid body
-        //RigidBody.run();
+        RigidBody.run();
 
         // arenstorf orbits
-        //Arenstorf.run();
+        Arenstorf.run();
 
         // vanderpol
-        //VanderPol.run();
+        VanderPol.run();
 
         System.out.println("Hello World!");
     }
